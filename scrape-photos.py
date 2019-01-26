32551f6fbf0029181a1cb93905377b247a2c18d8
Carter <cartercarlson94@gmail.com>
2019-01-26 12:59:46 -0600

    Create some helper functions

--- scrape-photos.py
+++ scrape-photos.py
@@ -1,7 +1,7 @@
-from os import remove
+from os import makedirs
 from os.path import expanduser, getctime
 from shutil import move
-import glob
+from glob import glob
 from PIL import Image
 from datetime import datetime
 import numpy as np
@@ -9,8 +9,14 @@ import pandas as pd
 from selenium import webdriver
 
 
+downloads_folder = expanduser('~/Downloads/*')
 login, password = pd.read_csv('login.csv').values[0]
-driver = webdriver.Chrome(executable_path="/home/carter/Documents/github/chromedriver")
+driver = webdriver.Chrome("chromedriver")
+
+def new_image():
+    list_of_files = glob(downloads_folder)
+    return max(list_of_files, key=getctime)
+
 
 def xpath(path, container=None):
     if not container:
@@ -18,9 +24,22 @@ def xpath(path, container=None):
     return container.find_elements_by_xpath('//' + path)
 
 
-driver.get('https://facebook.com/login')
+def rename_img(date_string, i):
+    # TODO: are single number days padded??
+    date_string_clean = date_string[date_string.find(',')+2:date_string.find(' at')] # Shorten the string to `January 01, 2018`
+    date = datetime.strptime(date_string_clean, '%B %d, %Y') # string type => datetime type
+    img_date = datetime.strftime(date, '%Y-%m-%d') # datetime object => `2018-01-31`
+    img = 'Photos-of-You/' + img_date + '-' + '0'*(4-len(str(i))) + '.jpg'
+
+    if img.exists():
+        return rename_img(date_string, i+1)
+    else:
+        return img, i
+
+
 
 # Log in to Facebook
+driver.get('https://facebook.com/login')
 xpath('input[@id="email"]').send_keys(login)
 xpath('input[@id="pass"]').send_keys(password)
 xpath('button[@id="loginbutton"]').click()
@@ -34,33 +53,31 @@ photos = xpath('div[@id="pagelet_timeline_medley_photos"]')
 
 ''' Download 'Photos of You' '''
 
+makedirs('Photos-of-You') # Make folder
 xpath('div[@class="tagWrapper"]').click() # click on first image
-xpath('a[@title="Next"]').click() # Go to next image
-xpath('span[contains(text(), "Options")]').click() # Click on options
-download_link = xpath('a[@data-action-type="download_photo"]', photos)[-1].get_attribute('href') # Get link to download
-driver.get(download_link)  # Download link
+photo_count = 1
+
+while True:
+
+    xpath('span[contains(text(), "Options")]').click() # Click on options
+    download_link = xpath('a[@data-action-type="download_photo"]', photos)[-1].get_attribute('href') # Get link to download
+    driver.get(download_link)  # Download link
+
+    fb_date = xpath('span[@id="fbPhotoSnowliftTimestamp"]/a/abbr').get_attribute('title') # Timestamp of image
+    img_name, photo_count = rename_img(fb_date, photo_count)
+
+
+
+
+    try:
+        xpath('a[@title="Next"]').click() # Go to next image
+    except
+        pass # Reached end of images
 
 
 
 # ------------------------------------------------------------------------------
 # Testing
-date = xpath('span[@id="fbPhotoSnowliftTimestamp"]/a/abbr').get_attribute('title')
-date_string = date[date.find(',')+2:date.find(' at')]
-date_date = datetime.strptime(date_string, '%B %d, %Y')
-date_string_new = datetime.strftime(date_date, '%Y-%m-%d')
-# NOTE: we'll add three 0's before .jpg, just in case >100 images were uploaded in a day
-
-downloads_folder = expanduser('~/Downloads/*')
-list_of_files = glob.glob(downloads_folder)
-latest_file = max(list_of_files, key=getctime)
-
-img = Image.open(latest_file)
-img.show()
-
-
-
-
-
 
 
 
