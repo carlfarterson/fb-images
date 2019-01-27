af837d7c7f08d9303060365c7a0b1b07eaae7df6
Carter <cartercarlson94@gmail.com>
2019-01-27 12:46:50 -0600

    Code works perfectly.  Time to add requirements

--- functions.py
+++ functions.py
@@ -11,40 +11,22 @@ driver = webdriver.Chrome(getcwd() + "/chromedriver")
 driver.get('https://facebook.com/login')
 
 
+def xpath(path, container=None):
+    if not container:
+        return driver.find_element_by_xpath('//' + path)
+    return container.find_elements_by_xpath('//' + path)
+
+
 def wait_for_closed_notification(t_0):
     if t_0 > 60:
         return "Unable to download images.  Ending Early"
     else:
         try:
             xpath('a[@title="Profile"]/span').click()
+            driver.minimize_window()
         except:
             sleep(1)
             return wait_for_closed_notification(t_0 + 1)
-
-
-def main(img_count):
-    xpath('span[contains(text(), "Options")]').click() # Click on options
-    sleep(1)
-    download_image() # download image
-    fb_date = xpath('span[@id="fbPhotoSnowliftTimestamp"]/a/abbr').get_attribute('title') # Timestamp of image
-    fb_date = validate_date(fb_date)
-    try:
-        filename, img_count = rename_img(fb_date, img_count)
-        move(most_recent_file('~/Downloads'), filename)
-        xpath('a[@title="Next"]').click() # Go to next image
-        return main(img_count)
-    except TypeError as error:
-        # Now we can delete the placeholder file
-        remove(placeholder)
-        remove(most_recent_file('~/Downloads'))
-        print("Download complete")
-        sleep(10)
-
-
-def xpath(path, container=None):
-    if not container:
-        return driver.find_element_by_xpath('//' + path)
-    return container.find_elements_by_xpath('//' + path)
 
 
 def download_image():
@@ -55,17 +37,6 @@ def download_image():
     return
 
 
-def validate_date(fb_date):
-    # Add 0 to the date string if it needs to
-    # Otherwise, return the string
-    day_10 = fb_date.find(', 20') - 2
-    if day_10 == ' ':
-        middle = day_10 - 1
-        return fb_date[:middle] + '0' + fb_date[middle:]
-    else:
-        return fb_date
-
-
 def most_recent_file(folder_name):
     folder = expanduser(folder_name + '/*')
     list_of_files = glob(folder)
@@ -73,23 +44,20 @@ def most_recent_file(folder_name):
     return file
 
 
-def rename_img(date_string, i):
-    date_string_clean = date_string[date_string.find(',')+2:date_string.find(' at')] # Shorten the string to `January 01, 2018`
-    img_date = datetime.strptime(date_string_clean, '%B %d, %Y') # string type => datetime type
+def rename_img(i):
 
-    last_img = most_recent_file('Photos-of-You')
-    last_img_date = datetime.strptime(last_img[last_img.find('/')+1: -8], '%Y-%m-%d')
+    fb_date = xpath('span[@id="fbPhotoSnowliftTimestamp"]/a/abbr').get_attribute('title') # Timestamp of image
 
-    if last_img_date.year > img_date.year:
-        return
+    # Shorten string from `Monday, Januaray 31, 2018 at 5:15pm`to `January 31, 2018`
+    fb_date = fb_date[fb_date.find(', ')+2: fb_date.find(' at')]
 
-    if last_img_date.day == img_date.day \
-        and last_img_date.month == img_date.month \
-        and last_img_date.year == img_date.year:
-        i += 1
-    else:
-        i = 1
+    day_10_digit = fb_date.find(', 20') - 2 # Add 0 to the date string if it's a single-digit # day
+    if fb_date[day_10_digit] == ' ':
+        fb_date = fb_date[:day_10_digit] + ' 0' + fb_date[day_10_digit+1:]
 
-    img_date_string = datetime.strftime(img_date, '%Y-%m-%d') # datetime object => `2018-01-31`
-    filename = 'Photos-of-You/' + img_date_string + '-' + '0'*(3-len(str(i))) + str(i) + '.jpg'
-    return filename, i
+    img_date_datetime = datetime.strptime(fb_date, '%B %d, %Y') # string type => datetime type
+    img_date_string = datetime.strftime(img_date_datetime, '%Y-%m-%d') # datetime object => `2018-01-31`
+    img_number = '0' * (4-len(str(i))) + str(i)
+
+    filename = 'Photos-of-You/' + img_number + '-' + img_date_string + '.jpg'
+    return filename
--- scrape-photos.py
+++ scrape-photos.py
@@ -5,28 +5,49 @@ import numpy as np
 import pandas as pd
 from functions import *
 
-
-# Log in to Facebook
 login, password = pd.read_csv('login.csv').values[0]
-xpath('input[@id="email"]').send_keys(login)
-xpath('input[@id="pass"]').send_keys(password)
-xpath('button[@id="loginbutton"]').click()
 
-# Click on profile
-t_0 = 0
-wait_for_closed_notification(t_0)
+def main(img_count):
+    xpath('span[contains(text(), "Options")]').click() # Click on options
+    sleep(1)
+    download_image() # download image
+    sleep(1)
 
-# Go to photos
-sleep(1)
-xpath('a[contains(text(), "Photos")]').click()
+    if img_count == 1:
+        # Get the name of the first file downloaded to ensure we're not downloading
+        # another file w/ the same name.
+        file = most_recent_file('~/Downloads')
+        first_img = file[file.find('Downloads/')+10:file.find('.jpg')]
+    elif first_img in most_recent_file('~/Downloads'):
+        remove(most_recent_file('~/Downloads'))
+        print('Download complete.')
+        driver.close()
+        return
 
-''' Download 'Photos of You' '''
-placeholder = 'Photos-of-You/2000-01-01-001.jpg'
+    # continue with main loop
+    filename = rename_img(img_count)
 
-makedirs('Photos-of-You') # Make folder
-open(placeholder, 'a').close() # Make placeholder img
-xpath('div[@class="tagWrapper"]').click() # click on first image
-sleep(1)
-img_count = 1
+    move(most_recent_file('~/Downloads'), filename)
+    xpath('a[@title="Next"]').click() # Go to next image
+    sleep(1)
+    return main(img_count + 1)
 
-main(img_count)
+
+if __name__ == '__main__':
+    # Log in to Facebook
+    xpath('input[@id="email"]').send_keys(login)
+    xpath('input[@id="pass"]').send_keys(password)
+    xpath('button[@id="loginbutton"]').click()
+
+    # Click on profile
+    t_0 = 0
+    wait_for_closed_notification(t_0)
+    # Go to photos
+    sleep(3)
+    xpath('a[contains(text(), "Photos")]').click()
+    sleep(5)
+    ''' Download 'Photos of You' '''
+    makedirs('Photos-of-You') # Make folder
+    xpath('div[@class="tagWrapper"]').click() # click on first image
+    sleep(1)
+    main(1)
